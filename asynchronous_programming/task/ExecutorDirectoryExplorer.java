222940e97d4c9bfad6cf7c16110725877df1c1cd
FPirazz <federico.pirazzoli@studio.unibo.it>
2023-05-01 01:44:16 +0200

    Implemented temporary solution for point 1

--- asynchronous_programming/task/ExecutorDirectoryExplorer.java
+++ asynchronous_programming/task/ExecutorDirectoryExplorer.java
@@ -1,28 +1,38 @@
 package task;
 
+import main.utility.Latch;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 public class ExecutorDirectoryExplorer {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
 
-        String directoryPath = "pcd";
+        String directoryPath = "input";
         int topN = 10;
         ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         List<JavaFileLength> fileLengths = new ArrayList<>();
-        searchJavaFiles(directoryPath, fileLengths, executor);
+        List<Future<?>> futureList = new ArrayList<>();
+
+        futureList.add(executor.submit(() -> {
+            searchJavaFiles(directoryPath, fileLengths, executor, futureList);
+        }));
+
+        Thread.sleep(2000);
+
+        boolean allDone = true;
+        for(Future<?> future : futureList){
+            allDone &= future.isDone(); // check if future is done
+        }
 
         executor.shutdown();
-        executor.awaitTermination(1, TimeUnit.MINUTES);
 
         List<JavaFileLength> longestFiles = fileLengths.stream()
                 .sorted(Comparator.comparingInt(JavaFileLength::getLength).reversed())
@@ -33,32 +43,31 @@ public class ExecutorDirectoryExplorer {
         longestFiles.forEach(System.out::println);
     }
 
-    private static void searchJavaFiles(String directoryPath, List<JavaFileLength> fileLengths, ExecutorService executor) {
-        executor.submit(() -> {
-            File directory = new File(directoryPath);
-            File[] files = directory.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    System.out.println(file);
-                    if (file.isFile() && file.getAbsoluteFile().getAbsolutePath().endsWith("java")) {
-                        executor.submit(() -> {
-                            try {
-                                System.out.println("|| Found file " + file.getAbsolutePath() + " ||");
-                                int length = Files.readAllLines(file.toPath()).stream().mapToInt(String::length).sum();
-                                fileLengths.add(new JavaFileLength(file.getName(), length));
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        });
-                    } else if (file.isDirectory()) {
-                        System.out.println("|| Found Directory " + file.getAbsolutePath() + " ||");
-                        searchJavaFiles(file.getPath(), fileLengths, executor);
-                    }
+    private static void searchJavaFiles(String directoryPath, List<JavaFileLength> fileLengths, ExecutorService executor, List<Future<?>> futureList) {
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                System.out.println(file);
+                if (file.isFile() && file.getAbsoluteFile().getAbsolutePath().endsWith("java")) {
+                    futureList.add(executor.submit(() -> {
+                        try {
+                            System.out.println("|| Found file " + file.getAbsolutePath() + " ||");
+                            int length = Files.readAllLines(file.toPath()).stream().mapToInt(String::length).sum();
+                            fileLengths.add(new JavaFileLength(file.getName(), length));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }));
+                } else if (file.isDirectory()) {
+                    System.out.println("|| Found Directory " + file.getAbsolutePath() + " ||");
+                    futureList.add(executor.submit(() -> {
+                        searchJavaFiles(file.getPath(), fileLengths, executor, futureList);
+                    }));
                 }
             }
-        });
+        }
     }
-
 
     private static class JavaFileLength {
         private final String fileName;
