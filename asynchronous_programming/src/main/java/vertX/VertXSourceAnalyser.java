715aba968f67d88d8f4935226d4ea2a73b3e28dc
FPirazz <federico.pirazzoli@studio.unibo.it>
2023-05-15 15:37:15 +0200

    Changed VertX solution without using Verticles, just Event Loop

--- asynchronous_programming/src/main/java/vertX/VertXSourceAnalyser.java
+++ asynchronous_programming/src/main/java/vertX/VertXSourceAnalyser.java
@@ -1,21 +1,25 @@
 package vertX;
 
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.file.FileSystem;
 import main.AbstractSourceAnalyser;
 import main.view.AnalyserView;
 import vertX.test.VertXDirectorySearch;
 import vertX.test.VertXFileRead;
 
 import java.io.File;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class VertXSourceAnalyser extends AbstractSourceAnalyser {
     Vertx vertx;
-    Queue<Future> futureList;
+    FileSystem fs;
+    Queue<Future> listFutures;
 
     public VertXSourceAnalyser() {
         vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(Runtime.getRuntime().availableProcessors()));
@@ -28,15 +32,18 @@ public class VertXSourceAnalyser extends AbstractSourceAnalyser {
                           final int maxL,
                           final int numTopFiles) throws InterruptedException {
         this.setParameters(directory, ranges, maxL, numTopFiles);
-        futureList.add(vertx.deployVerticle(new VertXDirectorySearchSource(futureList, directory)));
-        futureList.add(vertx.deployVerticle(new VertXFileReadSource(futureList,
-                view, false, intervals, topFiles, ranges, maxL, numTopFiles)));
 
+        vertx = Vertx.vertx();
+        fs = vertx.fileSystem();
 
-        while(futureList.size() > 0) {
-            futureList.remove().toCompletionStage().toCompletableFuture().join();
+        listFutures.add(Future.future(promise -> {
+            fileSearch(directoryPath, listFutures, fileLengths);
+            promise.complete();
+        }));
+
+        while(listFutures.size() > 0) {
+            listFutures.remove().toCompletionStage().toCompletableFuture().join();
         }
-        vertx.close();
 
         this.printTopFiles(topFiles);
         this.printIntervals(intervals);
@@ -70,5 +77,35 @@ public class VertXSourceAnalyser extends AbstractSourceAnalyser {
         vertx.close();
         view.changeState("Stopped");
     }
+
+
+    private void fileSearch(String directory, boolean updateGUI) {
+        Future<List<String>> directoryFuture = fs.readDir(directory);
+        listFutures.add(directoryFuture);
+        directoryFuture.onComplete((AsyncResult<List<String>> res) -> res.result().forEach(filePath -> {
+            File file = new File(filePath);
+            if (file.isFile() && file.getName().endsWith(".java")) {
+                listFutures.add(Future.future(promise -> {
+                    int numLines = this.countLines(file);
+                    this.updateIntervals(numLines);
+                    this.updateTopFiles(file, numLines);
+                    if(updateGUI) {
+                        listFutures.add(Future.future(promiseGUI -> {
+                            view.update(intervals, topFiles, ranges, maxL);
+                            promiseGUI.complete();
+                        }));
+                    }
+
+                }));
+            } else if (file.isDirectory()) {
+                listFutures.add(Future.future(promise -> {
+                    this.fileSearch(file.getAbsolutePath(), updateGUI);
+                    promise.complete();
+                }));
+            }
+        }));
+
+    }
+
 
 }
